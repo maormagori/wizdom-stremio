687f23da76e95eb3d5b531cbf5c9e0687177d188
maormagori <maormagori@gmail.com>
2020-10-08 17:56:37 +0300

    Switched to superagent from axios and managed to respond with a dummy srt file.

--- wizdom-stremio/addon.js
+++ wizdom-stremio/addon.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+const superagent = require('superagent');
+
 
 const { addonBuilder } = require("stremio-addon-sdk")
 
@@ -19,27 +20,49 @@ const manifest = {
 }
 const builder = new addonBuilder(manifest)
 
+//TODO: check status and change proxy.
 const wizDomain = "wizdom.xyz";
 
-const searchIMDB = (ImdbID, season=0 , episode=0, version=0) => {
-	// return new Promise((resolve, reject) => {
-		axios.get(`https://json.${wizDomain}/search.php?action=by_id&imdb=${ImdbID}&season=${season}&episode=${episode}&version=${version}`)
-			.then(res => {
-				console.log(res.data.length);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	// })
-}
+//TODO: add error handling.
+/**
+ * Searches wizdom for the imdb ID and returns an array of subtitle object.
+ * @param {Video Imdb id} ImdbID 
+ * @param {Season number} season 
+ * @param {Episode number} episode 
+ * @param {*} version
+ * @returns {https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/subtitles.md} subtitles
+ */
+const searchIMDB = async (ImdbID, season=0 , episode=0, version=0) => {
 
+	try{
+	const result = await superagent.get(`https://json.${wizDomain}/search.php?action=by_id&imdb=${ImdbID}&season=${season}&episode=${episode}&version=${version}`);
+	data = result.body;
+		const subtitles = [];
+		console.log("Found " + data.length + " subtitles.")
+		data.map((sub => {
+			subtitles.push({url: `https://mkvtoolnix.download/samples/vsshort-en.srt`, lang: "heb"})
+		}));
+		return subtitles;
+	} catch(err) {
+		console.log(err);
+	}
+
+	
+};
+
+
+// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineSubtitlesHandler.md
 builder.defineSubtitlesHandler(({type, id, extra}) => {
 	console.log("request for subtitles: "+type+" "+id)
-	//TODO: check tt
+
+	// TODO: check tt
 	titleInfo = id.split(":");
-	searchIMDB(titleInfo.shift(),titleInfo.shift(),titleInfo.shift(),titleInfo.shift());
-	// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineSubtitlesHandler.md
-	return Promise.resolve({ subtitles: [] })
+	return searchIMDB(titleInfo.shift(),titleInfo.shift(),titleInfo.shift(),titleInfo.shift()).then(wizSubtitles => {
+		console.log("reached then with " + wizSubtitles.length + " subs");
+		console.log(wizSubtitles)
+		return Promise.resolve({ subtitles: wizSubtitles });
+	});
+		
 })
 
 module.exports = builder.getInterface()
